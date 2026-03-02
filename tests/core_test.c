3bb3b772540ac73284d84a1bd3d460249eef8ecc
Tez <53151154+tezsezen@users.noreply.github.com>
2026-03-02 19:41:53 +0300

    Update core_test.c
    Temporary dummy test.

--- tests/core_test.c
+++ tests/core_test.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void test_true() {
+    if (1 != 1) {
+        fprintf(stderr, "FAIL: test_true\n");
+        exit(1);
+    }
+    printf("PASS: test_true\n");
+}
 
 int main() {
+    test_true();
     printf("All tests passed!\n");
     return 0;
 }
